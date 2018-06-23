9a57f0a484e130a8b692df1dfa2dd5094478827c
gudangcoding <40495749+gudangcoding@users.noreply.github.com>
2018-06-23 14:20:50 +0700

    Add files via upload

---
+++ modul/Home.php
@@ -0,0 +1,58 @@
+<div class="jumbotron">
+						<div class="container">
+							<h1 class="text-center">Selamat Datang Di Kursus Komputer Online</h1>
+							<h2 class="text-center">LPK Naura</h2>
+							<p>
+								Untuk Mengikuti kursus silahkan anda pilih sesuai keinginan anda dengan melihat detail dan silabus materi kursus di bawah ini
+							</p>
+							<div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+								<div class="thumbnail">
+									<img src="admin/asset/logo.jpg" alt="">
+									<div class="caption">
+										<h3 class="text-center">Web Master</h3>
+
+										
+											Kursus Web Master Adalah kursus yang mempelajari seluk Beluk keseluruhan tentang teknologi website
+									
+										<hr>
+											<a href="#" class="btn btn-success">Detail Materi</a>
+											<a href="#" class="btn btn-warning pull-right">Daftar</a>
+										</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+								<div class="thumbnail">
+									<img src="admin/asset/logo.jpg" alt="">
+									<div class="caption">
+										<h3 class="text-center">Web Desain</h3>
+
+										
+											Kursus Web Master Adalah kursus yang mempelajari seluk Beluk keseluruhan tentang teknologi website
+									
+										<hr>
+											<a href="#" class="btn btn-success">Detail Materi</a>
+											<a href="#" class="btn btn-warning pull-right">Daftar</a>
+										</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+								<div class="thumbnail">
+									<img src="admin/asset/logo.jpg" alt="">
+									<div class="caption">
+										<h3 class="text-center">Web Programming</h3>
+
+										
+											Kursus Web Master Adalah kursus yang mempelajari seluk Beluk keseluruhan tentang teknologi website
+									
+										<hr>
+											<a href="#" class="btn btn-success">Detail Materi</a>
+											<a href="#" class="btn btn-warning pull-right">Daftar</a>
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
